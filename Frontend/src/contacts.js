7ce1004d699efdb8fefa4fe0018fd4292165109c
Francisco <fkarkovich@gmail.com>
2021-09-13 21:26:37 -0300

    Added SWAL alerts

--- Frontend/src/contacts.js
+++ Frontend/src/contacts.js
@@ -1,28 +1,35 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import { ContactsForm, ContactsTable, Navbar, DeleteConfirmationModal } from './components/index.js';
 import { $ } from './libs/xQuery/xQuery.js';
-import { getContacts, deleteContact, createContact } from './services/contact.service.js';
+import { getContacts, deleteContact, createContact, bulkDeleteContact } from './services/contact.service.js';
 
-const $countSelectedTag = $('#count-selected-tag');
-const $rowsPerPageSelector = $('#rows-per-page-selector');
+const $CountSelectedTag = $('#count-selected-tag');
+const $RowsPerPageSelector = $('#rows-per-page-selector');
 // const $totalRows = $('#total-rows');
-const $previousTablePageBtn = $('#previous-table-page-btn');
-const $nextTablePageBtn = $('#next-table-page-btn');
-const $currentRowsTag = $('#current-rows-tag');
-const $totalRowsTag = $('#total-rows-tag');
-const $filterForm = $('#filter-form');
-const $filterQueryInput = $('#filter-query');
-const $header = $('#header');
-const $formAnchor = $('#form-anchor');
-const $contactsTableContainer = $('#contacts-table-container');
+const $PreviousTablePageBtn = $('#previous-table-page-btn');
+const $NextTablePageBtn = $('#next-table-page-btn');
+const $CurrentRowsTag = $('#current-rows-tag');
+const $TotalRowsTag = $('#total-rows-tag');
+const $FilterForm = $('#filter-form');
+const $FilterQueryInput = $('#filter-query');
+const $Header = $('#header');
+const $FormAnchor = $('#form-anchor');
+const $ContactsTableContainer = $('#contacts-table-container');
+const $AddButton = $('#add-button');
+const $BulkDeleteBtn = $('#bulk-delete-btn');
 
+let $ContactsForm;
 let $Modal;
 
-new Navbar($header, {links: [
+new Navbar($Header, {links: [
   {name: 'Contactos', path: './contacts.html', active: true},
   {name: 'Compañías', path: './companies.html'},
   {name: 'Usuarios', path: './users.html'},
   {name: 'Región / Ciudad', path: './regions.html'},
 ]});
+
+const $ContactsTable = new ContactsTable($ContactsTableContainer, { data: [] });
 
 function handleCountSelectedTagChange() {
   let count = 0;
@@ -41,11 +48,11 @@ function handleCountSelectedTagChange() {
     innerText = `${count} seleccionados`;
   }
     
-  $countSelectedTag.setInnerText(innerText);
+  $CountSelectedTag.setInnerText(innerText);
 }
 
 function updateRowsPerPage(newRowsPerPage) {
-  $contactsTable.setState({
+  $ContactsTable.setState({
     rowsPerPage: Number(newRowsPerPage),
     page: 1,
   });
@@ -53,15 +60,8 @@ function updateRowsPerPage(newRowsPerPage) {
 
 function handleFilterFormSubmit(e) {
   e.preventDefault();
-  $contactsTable.setState({filter: $filterQueryInput.value});
+  $ContactsTable.setState({filter: $FilterQueryInput.value});
 }
-
-const $contactsTable = new ContactsTable($contactsTableContainer, { data: [] });
-
-// const $contactsForm = new ContactsForm($formAnchor, {});
-// $contactsForm.unHide();
-
-
 
 async function loadContacts() {
   const contacts = await getContacts({fullState: true});
@@ -83,25 +83,28 @@ async function loadContacts() {
     contact.preferedChannels = contact?.contactChannels.map(cc => cc?.channel?.name);
   }
 
-  $contactsTable.setState({data: contacts});
+  $ContactsTable.setState({data: contacts});
 }
 
 async function deleteContactEventHandler(e) {
   const contactId = e.detail.id;
 
-  $Modal = new DeleteConfirmationModal($formAnchor, {
-    acceptCallback: () => console.log('accepted'),
+  $Modal = new DeleteConfirmationModal($FormAnchor, {
+    text: '¿Desea eliminar el contacto?',
+    acceptCallback: async () => {
+      const res = await deleteContact(contactId);
+
+      if (res.err) {
+        Swal.fire({icon: 'error', title: 'Error eliminando el contacto'});
+        return;
+      } 
+
+      const newData = $ContactsTable.state.data.filter(el => el.id !== contactId);
+      $ContactsTable.setState({data: newData});
+      Swal.fire({icon: 'success', title: 'Contacto eliminado exitosamente'});
+    },
     cancelCallback: () => console.log('cancelled')
   });
-  // const res = await deleteContact(contactId);
-  
-  // if (res.err) {
-  //   // mostrar alerta
-  //   return;
-  // } 
-
-  const newData = $contactsTable.state.data.filter(el => el.id !== contactId);
-  $contactsTable.setState({data: newData});
 }
 
 async function editContactEventHandler(e) {
@@ -110,22 +113,22 @@ async function editContactEventHandler(e) {
 }
 
 function previousTablePageBtnClickEventHandler() {
-  $contactsTable.switchPage('previous');
+  $ContactsTable.switchPage('previous');
 }
 
 function nextTablePageBtnClickEventHandler() {
-  $contactsTable.switchPage('next');
+  $ContactsTable.switchPage('next');
 }
 
 function updateTotalRowsEventHandler(e) {
   const totalRows = e.detail.totalRows;
-  $totalRowsTag.innerText = totalRows;
+  $TotalRowsTag.innerText = totalRows;
 }
 
 function updateCurrentRowsEventHandler(e) {
   const startRow = e.detail.startRow;
   const endRow = e.detail.endRow;
-  $currentRowsTag.innerText = `${startRow}-${endRow}`;
+  $CurrentRowsTag.innerText = `${startRow}-${endRow}`;
 }
 
 async function createContactEventHandler(e) {
@@ -134,10 +137,44 @@ async function createContactEventHandler(e) {
   console.log('createContactEvent', data);
   const res = await createContact(data);
   if (res.err) {
-    console.log('Error', res.err);
+    console.warn('Error', res.err);
+    Swal.fire({icon: 'error', title: 'Error al crear contacto'});
   } else {
-    alert('Contacto creado exitosamente');
+    await Swal.fire({icon: 'success', title: 'Contacto creado exitosamente'});
+    $ContactsForm.close();
   }
+}
+
+function handleAddButtonClick() {
+  $ContactsForm = new ContactsForm($FormAnchor, {});
+  $ContactsForm.unHide();
+}
+
+async function handleBulkDeleteButtonClick() {
+
+  const idArr = $ContactsTable.selectedRows?.map(row => row.id);
+
+  console.log(idArr);
+  if (!idArr || idArr.length === 0) return;
+  
+
+  $Modal = new DeleteConfirmationModal($FormAnchor, {
+    text: `¿Desea eliminar los contactos: ${idArr.join(', ')}?`,
+    acceptCallback: async () => {
+      const res = await bulkDeleteContact({contacts: idArr});
+
+      if (res.err) {
+        Swal.fire({icon: 'error', title: 'Error eliminando los contactos'});
+        return;
+      } 
+
+      const newData = $ContactsTable.state.data.filter(el => !idArr.includes(el.id));
+      $ContactsTable.setState({data: newData});
+      Swal.fire({icon: 'success', title: 'Contactos eliminados exitosamente'});
+    },
+    cancelCallback: () => console.log('cancelled')
+  });
+  
 }
 
 function initialize() {
@@ -148,11 +185,13 @@ function initialize() {
   document.addEventListener('update-total-rows', updateTotalRowsEventHandler);
   document.addEventListener('create-contact', createContactEventHandler);
   
-  $filterForm.addEventListener('submit', handleFilterFormSubmit);
-  $rowsPerPageSelector.addEventListener('change', (e) => updateRowsPerPage(e.target.value));
-  $previousTablePageBtn.addEventListener('click', previousTablePageBtnClickEventHandler);
-  $nextTablePageBtn.addEventListener('click', nextTablePageBtnClickEventHandler);
-  
+  $FilterForm.addEventListener('submit', handleFilterFormSubmit);
+  $RowsPerPageSelector.addEventListener('change', (e) => updateRowsPerPage(e.target.value));
+  $PreviousTablePageBtn.addEventListener('click', previousTablePageBtnClickEventHandler);
+  $NextTablePageBtn.addEventListener('click', nextTablePageBtnClickEventHandler);
+  $AddButton.addEventListener('click', handleAddButtonClick);
+  $BulkDeleteBtn.addEventListener('click', handleBulkDeleteButtonClick);
+
   document.dispatchEvent(new CustomEvent('update-count-selected-tag'));
 
   loadContacts();
